8c3c55bf2dc0b0c940b99f03442206d75a179bf9
Danko Kralski <d.kralski@student.fontys.nl>
2023-10-07 13:22:35 +0200

    fixed issue

--- src/main/java/s3/fontys/babysita/persistence/entity/PosterEntity.java
+++ src/main/java/s3/fontys/babysita/persistence/entity/PosterEntity.java
@@ -1,4 +1,26 @@
 package s3.fontys.babysita.persistence.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PosterEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String title;
+    private String description;
+    private String imageUrl;
+    private LocalDate eventDate;
+    private boolean isAppointed;
 }
