82a18ae0d4532c870e6ef70e0f23e100ff9b646f
Danko Kralski <d.kralski@student.fontys.nl>
2023-11-09 10:56:34 +0100

    docker file and jacoco in sonar

---
+++ src/test/java/s3/fontys/babysita/business/impl/BabysitterServiceImplTest.java
@@ -0,0 +1,16 @@
+package s3.fontys.babysita.business.impl;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BabysitterServiceImplTest {
+
+    @Test
+    void getBabysitter() {
+    }
+
+    @Test
+    void getAllBabysitters() {
+    }
+}
